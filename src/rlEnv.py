0354544e6244c1844f85b8c3eb7ddf0bb707c2a9
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2023-01-06 04:29:00 +0530

    test script to connect with the api

---
+++ src/rlEnv.py
@@ -0,0 +1,27 @@
+import airsim
+import os
+
+# connect to the AirSim simulator
+client = airsim.MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
+
+# Async methods returns Future. Call join() to wait for task to complete.
+client.takeoffAsync().join()
+client.moveToPositionAsync(-10, 10, -10, 5).join()
+
+# take images
+responses = client.simGetImages([
+    airsim.ImageRequest("0", airsim.ImageType.DepthVis),
+    airsim.ImageRequest("1", airsim.ImageType.DepthPlanar, True)])
+print('Retrieved images: %d', len(responses))
+
+# do something with the images
+for response in responses:
+    if response.pixels_as_float:
+        print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+        airsim.write_pfm(os.path.normpath('/temp/py1.pfm'), airsim.get_pfm_array(response))
+    else:
+        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+        airsim.write_file(os.path.normpath('/temp/py1.png'), response.image_data_uint8)
